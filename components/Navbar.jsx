4711dab19d3438a62e87620cff4a5f14aad9e375
lee14k <kmkovach@mtu.edu>
2024-03-12 22:14:51 -0400

    adding pages

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar =()=> {
+    return (
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Events</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
