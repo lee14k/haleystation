a2c5ea32ac5933b99561c3081058eeccc9990a40
lee14k <kmkovach@mtu.edu>
2024-04-01 18:23:48 -0400

    changing copy on rates page

--- pages/Rates.js
+++ pages/Rates.js
@@ -51,11 +51,7 @@ const Rates = () => {
                 space Complete set up of Tables Floor Length Round White Table
                 Linens Full bar with serviced bartender with purchase of alcohol
                 package. Use of in-house décor In-house coordination with
-                vendors for setup and timeliness of events
-                <h2>Capacity</h2>- Haley Station is ideal for intimate weddings
-                of 125 and less; hosting both ceremony & reception - 125 sit
-                comfortably dinner-style and ceremony-style in the main open
-                area - 225 max capacity, cocktail-style 6 Hour Packages - $595
+                vendors for setup and timeliness of events Hour Packages - $595
                 (Includes set up and clean up) Additional Hours available
                 $100/per hour Ideal for small events, showers, anniversary
                 parties, get-togethers, graduation parties, cocktail events,
@@ -65,30 +61,11 @@ const Rates = () => {
                 furniture; (1) lounge set, (3) bistro sets, (6) cocktail tables
                 Hourly Rate $100.00, Minimum of 3 hours
               </div>
-              <ul>
-                <li>
-                  A white interior with picture frame wall moldings and tons of
-                  natural light
-                </li>
-                <li>Large modern black windows with black grids</li>
-                <li>Expansive front windows</li>
-                <li>Dark moody tall tray ceiling</li>
-                <li>11 foot ceilings with accent moldings</li>
-                <li>Elegant dimmable chandeliers</li>
-                <li>Modern black ceiling fans</li>
-                <li>Custom statement doors, black with brass hardware</li>
-              </ul>
             </div>
-            <Link href="/Space">
-              {" "}
-              <button className="ctabutton bridalone brown-text px-8 py-6 text-3xl">
-                Learn More
-              </button>
-            </Link>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
